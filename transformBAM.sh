d6f76b19a6f6561a19a66f8d6b7f300f89147167
andrea_sboner <andrea.sboner.w@gmail.com>
2020-08-19 10:10:42 -0400

    splitting by RG; TODO convert and merge

--- transformBAM.sh
+++ transformBAM.sh
@@ -51,7 +51,6 @@ while [ "$1" != "" ]; do
 done
 
 logMsg "INFO" "--------------------- START Transformation -----------------------"
-logMsg "INFO" "Creating new header"
 if [[ ! -e "$FILE_IN" ]];then
     logMsg "ERROR" "Input file does not exists: ($FILE_IN)"
 fi
@@ -59,19 +58,33 @@ if [[ ! -e "$(dirname $FILE_IN)" ]];then
     logMsg "WARN" "Output folder does not exists: ($FILE_IN).\nCreating it now."
     mkdir -P $(dirname "$FILE_IN")
 fi
-samtools view -H $FILE_IN -@ 8 | sed "s%$PM_IN%$PM_OUT%g" > BAM.new.header.sam.txt
+logMsg "INFO" "Creating new header"
+samtools view -H "$FILE_IN" -@ 8 | sed "s%$PM_IN%$PM_OUT%g" > BAM.new.header.sam.txt
 logMsg "INFO" "Checking new header for PM IDs"
 if [[ $(grep -c $PM_IN BAM.new.header.sam.txt) -gt 0 ]];then
         logMsg "ERROR" "Found PM ID in header"
 fi
+logMsg "INFO" "Assigning the new header"
+samtools reheader -P BAM.new.header.sam.txt $FILE_IN > $FILE_OUT
+
+logMsg "INFO" "Checking the @RG info"
+mapfile -t RG_ARRAY < <(samtools view -H "$FILE_IN" | grep @RG)
+
+logMsg "INFO" "Splitting the BAM into the different @RGs"
+for RG in $RG_ARRAY
+do
+        samtools view -R $RG -bo $FILE_OUT"_"$RG".bam" "$FILE_IN"
+        RG_REPLACE=$(echo $RG | sed "s%$PM_IN%$PM_OUT")
+        samtools addreplacerg -r $RG_REPLACE -o $FILE_OUT"_"$RG_REPLACE".bam" $FILE_OUT"_"$RG".bam"
+done
+
 logMsg "INFO" "Checking BAM file for PM IDs"
-if [[ $(samtools view $FILE_IN | grep -c $PM_IN) -lt 1 ]];then
-        logMsg "INFO" "No PM IDs found in BAM file. Simply re-head the BAM file"
-        samtools reheader -P BAM.new.header.sam.txt $FILE_IN > $FILE_OUT
-else
+if [[ $(samtools view "$FILE_IN" | grep -c $PM_IN) -gt 0 ]];then
         logMsg "WARN" "Found PM IDs in BAM file. Need to change it. See an example:"
-        samtools view $FILE_IN | grep $PM_IN | head -n 2
-        cleanUp -3
+        samtools view "$FILE_IN" | grep $PM_IN | head -n 2
+        logMsg "INFO"
+else
+        logMsg "INFO" "No PM IDs found in BAM file. Simply re-head the BAM file"
 fi
 logMsg "INFO" "Checking the final output for PM IDs"
 if [[ $(samtools view -H  -@8 $FILE_OUT | grep -c $PM_IN) -gt 0 ]];then
