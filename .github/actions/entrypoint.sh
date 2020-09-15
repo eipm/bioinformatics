e598938527c2918aefe950514e8fdfcbf1a9f872
Andrea Sboner <ans2077@med.cornell.edu>
2020-09-15 15:15:22 -0400

    cleaning action and main workflow

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-echo "Hello"
 echo "Program: $0"
 echo "Parameter number: $#"
 for var in "$@"
@@ -11,3 +10,8 @@ echo "File In: $INPUT_FILE_IN"
 echo "Dir Out: $INPUT_DIR_OUT"
 echo "PM_IN  : $INPUT_PM_IN"
 echo "PM_OUT : $INPUT_PM_OUT"
+
+if [[ -e /usr/local/bin/transformBAM.sh ]];then
+    echo "Transforming the data"
+    transformBAM.sh --file-in "$INPUT_FILE_IN" --dir-out "$INPUT_DIR_OUT" --pm-in "$INPUT_PM_IN" --pm-out "$INPUT_PM_OUT"
+fi
