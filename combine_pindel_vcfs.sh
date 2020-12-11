7753fc49fd9370c227efd311054db40ce70aad91
Evan Fernandez <emf2009@med.cornell.edu>
2020-12-11 10:01:39 -0500

    Update combine_pindel_vcfs.sh
    Simplify to single line

--- combine_pindel_vcfs.sh
+++ combine_pindel_vcfs.sh
@@ -1,5 +1,1 @@
-DOI=$1
-FOIS=( $(ls $DOI/*vcf | egrep -e '(pindel_LI|pindel_RP|pindel_SI|pindel_TD|pindel_INV|pindel_INT_final|pindel_D)') )
-FINAL_FILE=$DOI"/pindel_all.vcf"
-VCFS_STR=for i in "${FOIS[*]}"; do echo "$i"; done
-vcf-concat -p $VCFS_STR >  $FINAL_FILE
+vcf-concat -p $(ls $DOI/*vcf | egrep -e '(pindel_LI|pindel_RP|pindel_SI|pindel_TD|pindel_INV|pindel_INT_final|pindel_D)') > ${1}/pindel_all.vcf
