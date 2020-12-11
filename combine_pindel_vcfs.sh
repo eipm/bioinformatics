a7d18785be6bd8b80f7d1f33bed45f16d8ec7ac3
Evan Fernandez <emf2009@med.cornell.edu>
2020-12-11 10:04:34 -0500

    Update combine_pindel_vcfs.sh

--- combine_pindel_vcfs.sh
+++ combine_pindel_vcfs.sh
@@ -1,1 +1,1 @@
-vcf-concat -p $(ls $DOI/*vcf | egrep -e '(pindel_LI|pindel_RP|pindel_SI|pindel_TD|pindel_INV|pindel_INT_final|pindel_D)') > ${1}/pindel_all.vcf
+vcf-concat -p $(ls ${1}/*vcf | egrep -e '(pindel_LI|pindel_RP|pindel_SI|pindel_TD|pindel_INV|pindel_INT_final|pindel_D)') > ${1}/pindel_all.vcf
